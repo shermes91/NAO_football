8567ee9896a3cd888fdfd7ed8365118cb579f361
NLI-LukasW <lwygasch@next-level-integration.com>
2017-12-20 14:40:08 +0100

    HeadPitch Added

--- src/nao/MoveNao.java
+++ src/nao/MoveNao.java
@@ -3,6 +3,7 @@ package nao;
 import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALMotion;
+import com.aldebaran.qi.helper.proxies.ALRobotPosture;
 import org.opencv.core.Point;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 public class MoveNao {
 
     ALMotion alMotion;
+    ALRobotPosture alRobotPosture;
     float x;
     float y;
 
@@ -49,7 +51,7 @@ public class MoveNao {
     }
 
     public enum Direction {
-        LEFT, RIGHT
+        LEFT, RIGHT, UP, DOWN
     }
 
     ;
@@ -57,6 +59,7 @@ public class MoveNao {
     public MoveNao(Session session) throws Exception {
         System.out.println("MOVE HEAD");
         alMotion = new ALMotion(session);
+        alRobotPosture = new ALRobotPosture(session);
         alMotion.clearStats();
         alMotion.moveInit();
         alMotion.setStiffnesses("Head", 1.0);
@@ -68,8 +71,9 @@ public class MoveNao {
         angles.add((float) 0);
         float speed = (float) 0.5;
         alMotion.setAngles(names, angles, speed);
-
-        //Thread.sleep(4000);
+        Thread.sleep(2000);
+        //standUp();
+        //moveForward();
         //alMotion.setStiffnesses("Head", 0);
 
     }
@@ -78,14 +82,16 @@ public class MoveNao {
         this.x = (float) p.x;
         this.y = (float) p.y;
         //Mitte 160 : 120
-        if (x < 140) turnRight();
-        else if(x > 180) turnLeft();
+        if (x < 140) turnHeadRight();
+        else if (x > 180) turnHeadLeft();
+        if (y < 50) turnHeadDown();
+        else if (x > 70) turnHeadUp();
     }
 
-    private void turnLeft() {
+    private void turnHeadLeft() {
         System.out.println("Turning Left");
         try {
-            alMotion.changeAngles("HeadYaw",-0.1,(float)0.05);
+            alMotion.changeAngles("HeadYaw", -0.1, (float) 0.05);
         } catch (CallError callError) {
             callError.printStackTrace();
         } catch (InterruptedException e) {
@@ -93,15 +99,59 @@ public class MoveNao {
         }
     }
 
-    private void turnRight() {
-        System.out.println("Truning Right");
+    private void turnHeadUp() {
+        System.out.println("HeadUp");
         try {
-            alMotion.changeAngles("HeadYaw",0.1,(float)0.05);
+            alMotion.changeAngles("HeadPitch", 0.1, (float) 0.05);
         } catch (CallError callError) {
             callError.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
+    private void turnHeadDown() {
+        System.out.println("HeadDown");
+        try {
+            alMotion.changeAngles("HeadPitch", -0.1, (float) 0.05);
+        } catch (CallError callError) {
+            callError.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void turnHeadRight() {
+        System.out.println("Turning Right");
+        try {
+            alMotion.changeAngles("HeadYaw", 0.1, (float) 0.05);
+        } catch (CallError callError) {
+            callError.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void standUp() {
+        System.out.println("Standing up");
+        try {
+            alRobotPosture.goToPosture("StandInit", (float) 0.7);
+        } catch (CallError callError) {
+            callError.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void moveForward() {
+        try {
+            alMotion.moveTo((float) 0.5, (float) 0, (float) 0);
+        } catch (CallError callError) {
+            callError.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
